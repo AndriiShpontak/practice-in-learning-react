ee210a5e926844606e3acb77573757eecca35d62
AndriiSH <andriishpontak@gmail.com>
2022-09-27 20:12:57 +0300

    useTransition, useDeferredValue

--- src/App.js
+++ src/App.js
@@ -1,120 +1,39 @@
-import React,{ Component } from 'react';
-import styled from 'styled-components';
-import BootstrapTest from './BootstrapTest';
-
-import './App.css';
-
-const EmpItem = styled.div`
-  padding: 20 px;
-  margin-bottom: 15px;
-  border-radius: 5px;
-  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
-  a {
-    display: block;
-    margin: 10px 0 10px 0;
-    color: ${props => props.active ? 'orange' : 'black'};
-  }
-  input {
-    display: block;
-    margin-top: 10px;
-  }
-`;
-
-const Header = styled.h2`
-  font-size: 20px;
-`;
-
-export const Button = styled.button`
-  display: block;
-  padding: 5px 15px;
-  background-color: gold;
-  border: 1px solid rgba(0,0,0, .2);
-  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
-`;
-
-class WhoAmI extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      years: 26,
-      position: ''
-    }
-    this.nextYear = this.nextYear.bind(this)
-  }
-
-  nextYear = () => {
-    this.setState(state => ({
-      years: state.years + 1
-    }))
-  }
-
-  commitInputChanges = (e, color) => {
-    console.log(color);
-    this.setState({
-      position: e.target.value
-    })
-  }
-  
-  render () {
-    const {name, surname, link} = this.props;
-    const {position, years} = this.state;
-    return(
-      <EmpItem active>
-        <Button onClick={this.nextYear}>+++</Button>
-        <Header>My name is {name}, surname - {surname}, 
-            age - {years}, 
-            position - {position}</Header>
-        <a href={link}>My profile</a>
-        <form>
-          <span>Enter the job title</span>
-          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
-        </form>
-      </EmpItem>
-    )
-  }
-}
-
-const Wrapper = styled.div`
-  width: 600px;
-  margin: 80px auto 0 auto;
-`;
-
-const DunamicGreating = (props) => {
-  return (
-    <div className={'mb-3 p-3 border border-' + props.color}>
-        {
-            React.Children.map(props.children, child => {
-                return React.cloneElement(child, {className: 'shadow p-3 m-3 border'})
-            })
-        }
-    </div>
-  )
-}
+import data from './data';
+import {useState, useMemo, useTransition, startTransition} from 'react';
+import { useDeferredValue } from 'react';
 
 function App() {
-  return (
-    <Wrapper>
-      
+    const [text, setText] = useState('');
+    const [posts, setPosts] = useState(data);
+    // const defferedValue = useDeferredValue(text);
+    const [isPanding, startTransition] = useTransition();
 
-      <BootstrapTest
-          left = {
-            <DunamicGreating color={'primary'}>
-              <h2>This well was hard</h2>
-              <h2>Hello world!</h2>
-             </DunamicGreating>
-          }
+    const filteredPosts = useMemo(() => {
+        return posts.filter(item => item.name.toLowerCase().includes(text));
+    }, [text]);
 
-          right = {
-            <DunamicGreating color={'primary'}>
-              <h2>Rigth!</h2>
-             </DunamicGreating>
-          }
-      />
+    const onValueChange = (e) => {
+      startTransition(() => {
+        setText(e.target.value);
+      })
+    }
 
-        <WhoAmI name='Andrii' surname="Shpontak" link="https://www.facebook.com/andrii.shpontak"/>
-        <WhoAmI name='Arthur' surname="Smitt" link="https://www.instagram.com"/>
-    </Wrapper>
-  );
+    return (
+        <>
+            <input value={text} type='text' onChange={onValueChange}/>
+
+            <hr/>
+
+            <div>
+                {isPanding ? <h4>Loading...</h4> : 
+                  filteredPosts.map(post => (
+                    <div key={post._id}>
+                        <h4>{post.name}</h4>
+                    </div>
+                ))} 
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Button} from './App';
+// import {Button} from './App';
+import BootstrapTest from './BootstrapTest';
 import styled from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const BigButton = styled(Button)`
-  margin: 0 auto;
-  width: 245px;
-  text-align: center;
-`;
+// const BigButton = styled(Button)`
+//   margin: 0 auto;
+//   width: 245px;
+//   text-align: center;
+// `;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
       <App/>
-      <BigButton as="a">Send a report</BigButton>
+      {/* <BigButton as="a">Send a report</BigButton> */}
+      <BootstrapTest/>
    </StrictMode>
 );
